df098b8fa224e0b58e351e68a989a7d93e991d9c
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-07-26 02:06:45 -0400

    Add script to fill db with dummy data
    Rather than using seeds.rb, which is for actual seed data.

---
+++ scripts/fill_db.rb
@@ -0,0 +1,9 @@
+
+Ingredient.create(name: 'Flour', is_available: false)
+Ingredient.create(name: 'Water', is_available: true)
+Ingredient.create(name: 'Sugar', is_available: true)
+Ingredient.create(name: 'Salt', is_available: true)
+Ingredient.create(name: 'Milk', is_available: false)
+
+Recipe.create(title: 'Sugar Cookies', description: 'Sweet and delicious', made_it_yet: true)
+Recipe.create(title: 'Pancakes', description: 'The best part of brunch', made_it_yet: true)
